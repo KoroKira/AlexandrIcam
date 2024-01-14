a85111b320d813e326e3b5c4e6b9255788af14a2
Guilhem Desarcy-Lemiere <guilhem.desarcy-lemiere@2027.icam.fr>
2024-01-14 20:15:06 +0100

    update

--- Apprentissage/Premiere_Annee/AII-Codage/exo15.py
+++
@@ -1,9 +0,0 @@
-taux_TVA = int(input("Quel est le taux de la TVA ? Donner la valeur en chiffre seulement, ex. 20.0 (valeurs possibles: 20%, 10%, 5.5%, 2.1%)"))
-
-prix_HT = input('Entrez un prix HT (entrez "o" pour terminer): ')
-
-while prix_HT != 'o':
-    prix_HT = float(prix_HT)
-    prix_TTC = prix_HT + (prix_HT * taux_TVA / 100)
-    print('Le prix TTC est de :{:.2f}'.format(prix_TTC))
-    prix_HT = input('Entrez un prix HT (entrez "o" pour terminer): ')
